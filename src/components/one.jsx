1f3ffa17f36abce580f8d59148517deddfb16761
DAVID NAYHOZ <316287317da@gmail.com>
2023-04-20 11:57:15 +0300

    moshe hchrish

--- src/components/one.jsx
+++ src/components/one.jsx
@@ -4,6 +4,8 @@ export default function one() {
   return (
     <div> hello world
         <div>dvid nehoz</div>
+        <div>moshe daymond</div>
+        <h1>daniel tolmsov</h1>
     </div>
   )
 }
